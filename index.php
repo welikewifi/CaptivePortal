ea9ee16b843b7f6d32345fc7d99eed9e83f24824
Braulio <barrerabraulio17@gmail.com>
2017-10-28 15:21:09 -0600

    Detect IOS
    Script to detect osx on the CPB

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php header( 'Location: /login.html' ) ;  ?>
+<?php
+/* Trying to get Apple to show the WiFi popup */
+if (!empty($_REQUEST['url']) && (strstr($_REQUEST['url'],'success.html') || strstr($_REQUEST['url'],'detect.html'))) {
+	echo '
+	Redirecting...
+	<script type="text/javascript">
+	window.location = "/login.html";
+	</script>';
+	exit;
+}
+header( 'Location: /login.html'.$_SERVER['QUERY_STRING']) ;  ?>
