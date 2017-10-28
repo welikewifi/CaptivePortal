08460fd788b905f1ab43c3475693a977f7a22633
Braulio <barrerabraulio17@gmail.com>
2017-10-28 15:30:14 -0600

    Add the osx detect code

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php header( 'Location: /login.html' ) ;  ?>
+<?php 
+if (!empty($_REQUEST['url']) && (strstr($_REQUEST['url'],'redirect.html') || strstr($_REQUEST['url'],'login.html'))) {
+	echo '
+	Redirecting...
+	<script type="text/javascript">
+	window.location = "/login.html";
+	</script>';
+	exit;
+}
+
+header("Location:/login.html?". $_SERVER['QUERY_STRING']);
